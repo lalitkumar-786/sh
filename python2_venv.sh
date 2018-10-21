b4fdd5ac05c48d8b837373c2b36ecd259dbcbe57
Lalit Kumar <lalitkumar.iiit@gmail.com>
2018-10-21 23:09:37 +0530

    A sh to istall py2 as virtualenv

---
+++ python2_venv.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#######################################################
+# The closer you see , the less you observe ---- Anonymous
+#######################################################
+#
+#  Run this script and install python 2 as virtual env without affecting your local env
+#
+
+sudo apt-get update
+
+read -p "Is python already installed ?(Y/N) : " option
+if [ "$option" = "Y"  ] || [ "$option" = "y"  ];
+then
+	echo "Taking path of already installed python"
+else
+	echo "*****Installing py 2********"
+	sudo apt install python2.7
+fi
+
+PY_PATH2=$( which python )
+cd $HOME
+echo "*********Setting env in HOME dir****************"
+sudo virtualenv -p $PY_PATH2 Project_py2
+
+echo "----------Venv successfully installed------------------"
+echo "Type : source Project_py2/bin/activate"
+
+
